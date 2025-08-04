304cb8071f6acc03e25bb2e8b33f2f1ab221129e
Kevin Rakholiya <anotherone3640@gmail.com>
2025-08-04 12:07:39 +0530

    order routes chang

--- src/orders/orderRoute.js
+++ src/orders/orderRoute.js
@@ -37,8 +37,8 @@ router.post("/create-checkout-session", async (req, res) => {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'payment',
-            success_url: "http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://treand-haven-frontend.vercel.app?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url: "https://treand-haven-frontend.vercel.app/cancel",
         })
         // console.log(session);
         // console.log("✅ Checkout Session Created:", session);
